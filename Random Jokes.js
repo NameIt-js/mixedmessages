86558aae6be20b1b6842c249790ac0b3eada9519
Natalia Morales Medina <85natmo58@gmail.com>
2021-05-26 21:37:28 -0500

    Working bad jokes

--- Random Jokes.js
+++ Random Jokes.js
@@ -3,12 +3,23 @@ const randomNumber = (thelength) => {
     return num;
 }
 
-console.log(randomNumber());
-
 const greetings = ['Hi', 'Hello', 'Hey!', "What's Up?", 'Greetings my friend'];
 const badJokes = ['What did the little mountain say to the bigger mountain? Hi Cliff!', "What do you call a hippie's wife? A Mississippi!", "What did the duck say when she bought a lipstick? Put it on my bill!", "What do bees do if they need a ride? Wait at the buzz stop!"];
-const goodbyes = ['See you next time mate', 'Goodbye my friend', 'Next time is your tourn', 'Bye, I wait untill you call me again'];
+const goodbyes = ['See you next time mate', 'Goodbye my friend', 'Next time is your turn', 'Bye, I wait untill you call me again'];
 
-console.log(greetings[0]);
-console.log(badJokes[0]);
-console.log(goodbyes[0]);
+
+const theLine = (arr) => {
+    let number = randomNumber(arr.length);
+    return arr[number];
+}
+
+const theScript = (line1, line2, line3) =>{
+    let a = theLine(line1);
+    let b = theLine(line2);
+    let c = theLine(line3);
+    return `${a} Wanna hear a joke? 
+${b}.
+${c}. `;
+}
+
+console.log(theScript(greetings,badJokes,goodbyes));
